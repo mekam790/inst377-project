9198727d5f388cdac9fabb509b672ce0493c7a14
Michelle Akem <75742067+mekam790@users.noreply.github.com>
2025-12-14 17:30:41 -0500

    adding more status tracking on html

--- src/components/Tracking.jsx
+++ src/components/Tracking.jsx
@@ -18,10 +18,22 @@ const Tracking = () => {
   const showTime =
     date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
+    const categoryDisplay = document.getElementById("current-category");
+  // function when form is submitted
   const submitTracking = async (data) => {
+    const timesheet = document.getElementById("timesheet").getElementsByTagName('tbody')[0];
     data.preventDefault();
     const timeData = new FormData(data.target);
-    console.log([...timeData.entries()]);
+
+    const timeBlock = timesheet.insertRow();
+    const timeCell = timeBlock.insertCell(0);
+    const activityCell = timeBlock.insertCell(1);
+    const categoryCell = timeBlock.insertCell(2);
+    timeCell.innerHTML = timeData.get("from") + " - " + timeData.get("to");
+    activityCell.innerHTML = timeData.get("activity");
+    categoryCell.innerHTML = timeData.get("category");
+
+    categoryDisplay.innerHTML = timeData.get("category")
     const timeEntry = {
       from: timeData.get("from"),
       to: timeData.get("to"),
@@ -65,50 +77,71 @@ const Tracking = () => {
         <h3>{showTime}</h3>
         <h5 id="current-category"></h5>
       </div>
-      <form id="tracking-form" onSubmit={submitTracking}>
-        {/* can have min and max attributes, use to set restrictions when user first inputs sleep time*/}
-        <label for="from">Time Block Start:</label>
-        <input type="time" id="from" name="from" step="1800" required />
-        <br />
-        <label for="to">Time Block End:</label>
-        <input type="time" id="to" name="to" step="1800" required />
-        <br />
-        <label for="activity">Activity:</label>
-        <input type="text" id="activity" name="activity" required />
-        <br />
-        <label for="category">Category:</label>
-        <select id="category" name="category" required>
-          <option value="career">Career</option>
-          <option value="financial">Financial</option>
-          <option value="spiritual">Spiritual</option>
-          <option value="physical">Physical</option>
-          <option value="intellectual">Intellectual</option>
-          <option value="family">Family</option>
-          <option value="social">Social</option>
-        </select>
-        <br />
-        <label for="fun_level">Fun Level (1-4):</label>
-        <input
-          type="number"
-          id="fun_level"
-          name="fun_level"
-          min="1"
-          max="4"
-          required
-        />
-        <br />
-        <label for="meaning_level">Meaning Level (1-4):</label>
-        <input
-          type="number"
-          id="meaning_level"
-          name="meaning_level"
-          min="1"
-          max="4"
-          required
-        />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      <div id="form">
+        <form id="tracking-form" onSubmit={submitTracking}>
+          {/* can have min and max attributes, use to set restrictions when user first inputs sleep time*/}
+          <label for="from">Time Block Start:</label>
+          <input type="time" id="from" name="from" step="1800" required />
+          <br />
+          <label for="to">Time Block End:</label>
+          <input type="time" id="to" name="to" step="1800" required />
+          <br />
+          <label for="activity">Activity:</label>
+          <input type="text" id="activity" name="activity" required />
+          <br />
+          <label for="category">Category:</label>
+          <select id="category" name="category" required>
+            <option value="career">Career</option>
+            <option value="financial">Financial</option>
+            <option value="spiritual">Spiritual</option>
+            <option value="physical">Physical</option>
+            <option value="intellectual">Intellectual</option>
+            <option value="family">Family</option>
+            <option value="social">Social</option>
+          </select>
+          <br />
+          <label for="fun_level">Fun Level (1-4):</label>
+          <input
+            type="number"
+            id="fun_level"
+            name="fun_level"
+            min="1"
+            max="4"
+            required
+          />
+          <br />
+          <label for="meaning_level">Meaning Level (1-4):</label>
+          <input
+            type="number"
+            id="meaning_level"
+            name="meaning_level"
+            min="1"
+            max="4"
+            required
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div id="timesheetbox">
+        <h2>Your Time Entries</h2>
+        <div id="entries-list">
+            <table id="timesheet">
+                <thead>
+                    <tr>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Activity</th>
+                    <th>Category</th>
+                    <th>Fun Level</th>
+                    <th>Meaning Level</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+      </div>
     </>
   );
 };
