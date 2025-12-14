a8d36afc1751d53e8bf202e2af11227f46642681
Michelle Akem <75742067+mekam790@users.noreply.github.com>
2025-12-14 12:21:50 -0500

    starting tracking

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import '../App.css';
-import Navbar from './Navbar';
+import React from "react";
+import "../App.css";
+import Navbar from "./Navbar";
 
 const Home = () => {
-    return (
-        <div class="header">
-          <div class="title">
-            <h1>The Optimization Problem</h1>
-          </div>
-          <Navbar />
-        </div>
-    )
-}
+  return (
+    <div class="header">
+      <div class="title">
+        <h1>The Optimization Problem</h1>
+      </div>
+      <Navbar />
+      <div id="intro">
+        <p>
+          This website is meant to help you track your time to learn how to
+          spend it more meaningfully. Because time is a precious resource that
+          we can't get back, it's important that it's used intentionally. Make
+          your way to the tracking page to start your 2-week sprint.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Tracking.jsx
+++ src/components/Tracking.jsx
@@ -3,13 +3,28 @@ import "../App.css";
 import Navbar from "./Navbar";
 
 const Tracking = () => {
+  // get current date
+  const date = new Date();
+  const showTime =
+    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
   return (
-    <div class="header">
-      <div class="title">
-        <h1>Track Time</h1>
+    <>
+      <div class="header">
+        <div class="title">
+          <h1>Track Time</h1>
+        </div>
+        <Navbar />
       </div>
-      <Navbar />
-    </div>
+      <div id="tracking-intro">
+        <p>
+          Use the form below to track how much time you spend on different
+          activities throughout your day. Select the time block, log your
+          activity in a few words, the time category, and the level of fun and
+          meaning on a scale of 1-4.
+        </p>
+      </div>
+    </>
   );
 };
 
