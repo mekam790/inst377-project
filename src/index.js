9040dc8da31a7dc60f66629b159c28f758cdd87d
Michelle Akem <75742067+mekam790@users.noreply.github.com>
2025-12-15 19:16:44 -0500

    praying

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { sendToVercelAnalytics } from "./vitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { sendToVercelAnalytics } from './vitals';
 import { createRoot } from "react-dom/client";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
 
